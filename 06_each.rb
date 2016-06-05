99dd9abea23fadf4e889cc30680a5a9cd27a3c0b
Ian Whitney <whit0694@umn.edu>
2016-06-05 08:32:24 -0500

    each

---
+++ 06_each.rb
@@ -0,0 +1,55 @@
+# Each
+#
+# Displaying each of the student's name was kind of a pain, right? We had to know how big our array was, then display each item. If we had to add or remove a student, what would we have to do?
+#
+# We want to do the same thing with each name. Ruby offers us an easy way to do this, called `each`.
+#
+#
+# Type
+# names = ["Ian", "Fred", "Suma", "Ben", "Kyle", "Glenn"]
+# names.each do |name|
+#   puts name
+# end
+#
+#
+#
+# Let's look into what's going on here.
+#
+# - We have a bunch of `names`
+# - We put each name in variable, `name`
+# - We then display the name
+# - When we display the last name, we're done
+#
+# `each` is incredibly powerful and is part of Ruby's best features. So let's play with it some more.
+#
+#
+#
+# Exercise One
+#
+# Let's go back to our t-shirt example.
+# You have three shirts
+#   - New Shirt
+#   - Howling Wolf
+#   - Cool Band
+#
+# Display
+#  "I'm wearing New Shirt"
+#  "I'm wearing Howling Wolf"
+#  "I'm wearing Cool Band"
+#
+# Hints
+#   - Start off with
+#     drawer = ["New Band", "Howling Wolf", "Cool Band"]
+#   - Use `each`
+#   - You can use `+` to stick two strings together
+#
+# Exercise Two
+#
+# Put all of our names in an array.
+# Display each name in ALL CAPS
+#
+# Exercise Three
+#
+# Put all of our names in an array.
+# Display each of our names.
+# If you are displaying your name, display it in ALL CAPS
